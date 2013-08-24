7a784367131f4387873aca00836cb97a4beb4fa4
Pepe <ubuntu@ip-172-31-32-221.us-west-2.compute.internal>
2013-08-24 17:28:28 +0000

    Buffer  ver buffer

--- web.js
+++ web.js
@@ -4,7 +4,9 @@ var app = express();
 var fs = require('fs');
 
 //----------------------------
-var buffer = new Buffer( 100); 
+// var buffer = new Buffer( 100);
+var buffer = new Buffer( 'Toma con el buffer', 'utf-8' );
+ 
 // buffer.write( fs.readFileSync('index.html') );
 
 //----------------------------/
@@ -13,9 +15,8 @@ var buffer = new Buffer( 100);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send( fs.readFileSync('index.html','utf8') );
-// response.send( buffer.toString('utf-8') );
-
+//  response.send( fs.readFileSync('index.html','utf8') );
+response.send( buffer.toString('utf-8',0,18) );
 });
 
 var port = process.env.PORT || 5000;
