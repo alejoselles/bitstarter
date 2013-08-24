0f55cfbae0870386edea148483b045d62455a41d
Pepe <ubuntu@ip-172-31-32-221.us-west-2.compute.internal>
2013-08-24 17:37:49 +0000

    Buffer con write y utf8

--- web.js
+++ web.js
@@ -4,10 +4,11 @@ var app = express();
 var fs = require('fs');
 
 //----------------------------
-// var buffer = new Buffer( 100);
-var buffer = new Buffer( 'Toma con el buffer Gordo', 'utf-8' );
+var buffer = new Buffer( 100);// ok
+// var buffer = new Buffer( 'Toma con el buffer', 'utf-8' ); // ok
  
 // buffer.write( fs.readFileSync('index.html') );
+buffer.write ('Esto es con el write', 'utf-8');
 
 //----------------------------/
 
@@ -16,7 +17,9 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send( fs.readFileSync('index.html','utf8') );
-response.send( buffer.toString('utf-8') );
+    // response.send( buffer.toString('utf-8',0,18) );// ok
+    response.send( buffer.toString('utf-8') );// ok too
+
 });
 
 var port = process.env.PORT || 5000;
