b3cbef0796e811b52763042a8c22101e48aff2cc
Pepe <ubuntu@ip-172-31-32-221.us-west-2.compute.internal>
2013-08-24 16:48:32 +0000

    Volvemos a la version sin buffer

--- web.js
+++ web.js
@@ -1,18 +1,12 @@
 var express = require('express');
 var app = express();
+
 var fs = require('fs');
-
-#-----------
-# var buffer = new Buffer( 100);  
-
-# buffer.write( fs.readFileSync('index.html') );
-#----------/
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send( fs.readFileSync('index.html','utf8') );
-# response.send( buffer.toString('utf-8') );# This tttt work
 });
 
 var port = process.env.PORT || 5000;
